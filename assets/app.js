07cbaca9ed2bc9421ffa4c9bbf3311c340f1279c
JuanMa Garrido <JuanMa.Garrido@gmail.com>
2017-02-25 08:03:45 +0100

    Updates

--- assets/app.js
+++ assets/app.js
@@ -5,7 +5,7 @@ function loadAdmin () {
       const type = 'GET'
       const url = `https://webtask.it.auth0.com/api/run/wt-76fcf2ae5936171bf52221b4075c11a7-0/newsletter/subscribers?webtask_no_cache=1`
       const headers = { Authorization: `Bearer ${localStorage.getItem('token')}` }
-      $.ajax({ url })
+      $.ajax({ url, type, headers })
         .done(data => {
           const html = data.map(mail => `<h4>${mail}</h4>`).join('')
           $('#subscribers').html(html)
